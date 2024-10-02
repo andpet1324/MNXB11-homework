c27d1feb8320c2cd0757a1271d44b9a1c2db3a4f
Andreas Pettersson <an6826pe-s@student.lu.se>
2024-10-02 15:35:18 +0200

    changed zero1 to int_val. Created a function to replace the while loop. Changed the while loop to a for loop with an index i instead of the previous value zero2 (val7).

--- homework6/main.cxx
+++ homework6/main.cxx
@@ -1,22 +1,28 @@
 #include <iostream>
 #include <string>
+auto func(int int_val, std::string first_arg) {
+  for (int i = 0; i >= 0; i++) {
+    int_val += first_arg[i];
+    if (i >= static_cast<int>(first_arg.size())) {
+        break;
+    }
+  }
+    return int_val;
+}
+
 int main(int number_of_args, char *arg_container[]) {
   if (number_of_args == 3) {
     std::string program_name{arg_container[0]};
-    char first_arg_first_letter{*(arg_container[1])};
+    auto first_arg_first_letter{*(arg_container[1])};
     auto program_name_val{program_name.size()};
     auto second_arg_string{std::atoi(arg_container[2])};
-    auto zero1{0};
+    auto int_val{0};
     std::string first_arg{arg_container[1]};
-    while (true) {
-      zero1 += first_arg[zero1
-++];
-      if (zero1
- >= static_cast<int>(first_arg.size())) {
-        break;
-      }
-    }
-    if ((zero1 ^ first_arg_first_letter * 3) << (program_name_val & 0x1f) == second_arg_string) {
+
+    int_val = func(int_val, first_arg);
+    
+    std::cout << int_val << std::endl;
+    if ((int_val ^ first_arg_first_letter * 3) << (program_name_val & 0x1f) == second_arg_string) {
       std::cout << "Correct!" << std::endl;
     } else {
       std::cout << "Wrong!" << std::endl;
