29b1cd3a39085bac06f94b0a740e2cebdab73fe1
Andreas Pettersson <an6826pe-s@student.lu.se>
2024-10-02 14:54:43 +0200

    changed variable names

--- homework6/main.cxx
+++ homework6/main.cxx
@@ -1,21 +1,23 @@
 #include <iostream>
 #include <string>
-int main(int number_of_arguments, char *argument_container[]) {
-  if (number_of_arguments == 3) {
-    std::string var2{argument_container[0]};
-    auto var3{*(argument_container[1])};
-    auto var4{var2.size()};
-    auto var5{std::atoi(argument_container[2])};
-    auto var6{0};
-    auto var7{0};
-    std::string var8{argument_container[1]};
+int main(int number_of_args, char *arg_container[]) {
+  if (number_of_args == 3) {
+    std::string program_name{arg_container[0]};
+    char first_arg_first_letter{*(arg_container[1])};
+    auto program_name_val{program_name.size()};
+    auto second_arg_string{std::atoi(arg_container[2])};
+    auto zero1{0};
+    auto zero2{0};
+    std::string first_arg{arg_container[1]};
     while (true) {
-      var6 += var8[var7++];
-      if (var7 >= static_cast<int>(var8.size())) {
+      zero1 += first_arg[zero2
+++];
+      if (zero2
+ >= static_cast<int>(first_arg.size())) {
         break;
       }
     }
-    if ((var6 ^ var3 * 3) << (var4 & 0x1f) == var5) {
+    if ((zero1 ^ first_arg_first_letter * 3) << (program_name_val & 0x1f) == second_arg_string) {
       std::cout << "Correct!" << std::endl;
     } else {
       std::cout << "Wrong!" << std::endl;
